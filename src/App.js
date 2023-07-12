de9cd71d58c373ae62f8e82c35f92ac1b7107d82
Ibtisam Anwar <ibtisamanwar008@gmail.com>
2023-07-12 23:25:53 +0500

    Fixed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
+import React from "react";
 import { ReactMediaRecorder } from "react-media-recorder";
 
 const RecordView = () => (
   <div>
     <ReactMediaRecorder
       screen
-      render={({ status, startRecording, stopRecording, mediaBlobUrl }) => (
-        <div>
-          <p>{status}</p>
-          <button onClick={startRecording}>Start Recording</button>
-          <button onClick={stopRecording}>Stop Recording</button>
-          {status === "stopped" && (
-            <video src={mediaBlobUrl} controls autoPlay loop />
-          )}
-        </div>
-      )}
-      mimeType="video/webm"
+      render={({ status, startRecording, stopRecording, mediaBlobUrl }) => {
+        const handleDownload = () => {
+          if (mediaBlobUrl) {
+            const link = document.createElement("a");
+            link.href = mediaBlobUrl;
+            link.download = "recorded-video.mp4";
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+          }
+        };
+
+        if (status === "stopped") {
+          handleDownload();
+        }
+
+        return (
+          <div>
+            <p>{status}</p>
+            <button onClick={startRecording}>Start Recording</button>
+            <button onClick={stopRecording}>Stop Recording</button>
+            {status === "stopped" && (
+              <video src={mediaBlobUrl} controls autoPlay loop />
+            )}
+          </div>
+        );
+      }}
+      mimeType="video/mp4"
     />
   </div>
 );
